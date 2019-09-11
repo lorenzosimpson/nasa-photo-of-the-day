1fcad421b14790c690cce01e48ffe5d952d19c91
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-09-11 15:20:29 -0400

    First commit, adds dependencies and creates Header component

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header-container">
+            <h1>NASA APOD</h1>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+export default Header;

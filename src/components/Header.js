9b9015a0233e65de3d940a60f3572974584dadb3
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-09-12 15:57:47 -0400

    Adds gradient to background, spaces image title

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,11 @@ import styled from 'styled-components';
 
 const Title = styled.h1`
     color: #2e2e2e;
-    font-family: 'Open Sans', sans-serif;
     font-size: 4rem;
     letter-spacing: 2rem;
+    padding-bottom: 2rem;
+    border-bottom: 1px solid black;
+    
 `
 
 
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const ImageTitleStyled = styled.h2`
+    font-size: 2rem;
+    letter-spacing: 0.6rem;
+`
 
 const Image = props => {
     
     return (
         <div>
-            <h2>{props.title}</h2>
+            <ImageTitleStyled>{props.title.toUpperCase()}</ImageTitleStyled>
             <p>{props.date}</p>
             <img src={props.url} alt={props.title}></img>
             <p>{props.explanation}</p>

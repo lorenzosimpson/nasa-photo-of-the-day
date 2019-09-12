02db42f56ac29a9d6207d2679de1aec736c31a12
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-09-12 15:35:50 -0400

    import reactstrap, styled components and font-family

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const Title = styled.h1`
+    color: #2e2e2e;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 4rem;
+    letter-spacing: 2rem;
+`
+
 
 function Header() {
     return (
         <div className="header-container">
-            <h1>NASA APOD</h1>
+            <Title>NASA APOD</Title>
         </div>
     )
 }
